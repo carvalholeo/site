d6d5d68e4e4f4cc2aefd70f7084fd2121de16124
Leonardo Carvalho <carvalho.csleo@gmail.com>
2019-04-27 06:38:21 -0300

    Fix navbar layout and functionality

--- resources/views/layout/component_navbar.blade.php
+++ resources/views/layout/component_navbar.blade.php
@@ -1,18 +1,78 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark rounded">
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
-  <div class="collapse navbar-collapse" id="navbar">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item @if($current=='home') active @endif ">
-        <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item @if($current=='produtos') active @endif ">
-        <a class="nav-link" href="/produtos">Produtos</a>
-      </li>
-      <li class="nav-item @if($current=='categorias') active @endif ">
-        <a class="nav-link" href="/categorias">Categorias</a>
-      </li>
-  </div>
-</nav>
+<div class="navbar navbar-expand-md navbar-dark bg-dark mb-4" role="navigation">
+        <a class="navbar-brand" href="#">Léo Carvalho</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse"
+            aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item @if($current=='home') active @endif ">
+                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item @if($current=='produtos') active @endif ">
+                    <a class="nav-link" href="http://fontenele.github.io/bootstrap-navbar-dropdowns/"
+                        target="_blank">Github</a>
+                </li>
+                <li class="nav-item @if($current=='produtos') active @endif ">
+                    <a class="nav-link disabled" href="#">Disabled</a>
+                </li>
+                <li class="nav-item dropdown @if($current=='produtos') active @endif ">
+                    <a class="nav-link dropdown-toggle" id="dropdown1" data-toggle="dropdown" aria-haspopup="true"
+                        aria-expanded="false">Dropdown1</a>
+                    <ul class="dropdown-menu" aria-labelledby="dropdown1">
+                        <li class="dropdown-item" href="#"><a>Action 1</a></li>
+                        <li class="dropdown-item dropdown">
+                            <a class="dropdown-toggle" id="dropdown1-1" data-toggle="dropdown" aria-haspopup="true"
+                                aria-expanded="false">Dropdown1.1</a>
+                            <ul class="dropdown-menu" aria-labelledby="dropdown1-1">
+                                <li class="dropdown-item" href="#"><a>Action 1.1</a></li>
+                                <li class="dropdown-item dropdown">
+                                    <a class="dropdown-toggle" id="dropdown1-1-1" data-toggle="dropdown"
+                                        aria-haspopup="true" aria-expanded="false">Dropdown1.1.1</a>
+                                    <ul class="dropdown-menu" aria-labelledby="dropdown1-1-1">
+                                        <li class="dropdown-item" href="#"><a>Action 1.1.1</a></li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                </li>
+                <li class="nav-item dropdown @if($current=='produtos') active @endif ">
+                    <a class="nav-link dropdown-toggle" id="dropdown2" data-toggle="dropdown" aria-haspopup="true"
+                        aria-expanded="false">Dropdown2</a>
+                    <ul class="dropdown-menu" aria-labelledby="dropdown2">
+                        <li class="dropdown-item" href="#"><a>Action 2 A</a></li>
+                        <li class="dropdown-item" href="#"><a>Action 2 B</a></li>
+                        <li class="dropdown-item" href="#"><a>Action 2 C</a></li>
+                        <li class="dropdown-item dropdown">
+                            <a class="dropdown-toggle" id="dropdown2-1" data-toggle="dropdown" aria-haspopup="true"
+                                aria-expanded="false">Dropdown2.1</a>
+                            <ul class="dropdown-menu" aria-labelledby="dropdown2-1">
+                                <li class="dropdown-item" href="#"><a>Action 2.1 A</a></li>
+                                <li class="dropdown-item" href="#"><a>Action 2.1 B</a></li>
+                                <li class="dropdown-item" href="#"><a>Action 2.1 C</a></li>
+                                <li class="dropdown-item dropdown">
+                                    <a class="dropdown-toggle" id="dropdown2-1-1" data-toggle="dropdown"
+                                        aria-haspopup="true" aria-expanded="false">Dropdown2.1.1</a>
+                                    <ul class="dropdown-menu" aria-labelledby="dropdown2-1-1">
+                                        <li class="dropdown-item" href="#"><a>Action 2.1.1 A</a></li>
+                                        <li class="dropdown-item" href="#"><a>Action 2.1.1 B</a></li>
+                                        <li class="dropdown-item" href="#"><a>Action 2.1.1 C</a></li>
+                                    </ul>
+                                </li>
+                                <li class="dropdown-item dropdown">
+                                    <a class="dropdown-toggle" id="dropdown2-1-2" data-toggle="dropdown"
+                                        aria-haspopup="true" aria-expanded="false">Dropdown2.1.2</a>
+                                    <ul class="dropdown-menu" aria-labelledby="dropdown2-1-2">
+                                        <li class="dropdown-item" href="#"><a>Action 2.1.2 A</a></li>
+                                        <li class="dropdown-item" href="#"><a>Action 2.1.2 B</a></li>
+                                        <li class="dropdown-item" href="#"><a>Action 2.1.2 C</a></li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </div>
