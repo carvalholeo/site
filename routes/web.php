00f49bdec86a59cef6b10b54d6f244c858d19fbb
Leonardo Carvalho <carvalho.csleo@gmail.com>
2019-05-01 23:53:24 -0300

    Add Controllers and routes

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('/contact', 'ContactController');
+Route::resource('/resume', 'ResumeController');
+Route::resource('/portfolio', 'PortfolioController');
