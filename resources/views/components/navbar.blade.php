02ea30ea788c3f685e13e7923ea40ce07059bbee
Leonardo Carvalho <carvalho.csleo@gmail.com>
2019-05-05 14:08:05 -0300

    Fix navbar functionality

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,75 +1,72 @@
-<div class="navbar navbar-expand-md navbar-dark bg-dark mb-4" role="navigation">
-        <a class="navbar-brand" href="#">Léo Carvalho</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse"
-            aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarCollapse">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item @if($current=='home') active @endif ">
-                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item @if($current=='produtos') active @endif ">
-                    <a class="nav-link disabled" href="#">Disabled</a>
-                </li>
-                <li class="nav-item dropdown @if($current=='resume') active @endif ">
-                    <a class="nav-link dropdown-toggle" id="dropdown1" data-toggle="dropdown" aria-haspopup="true"
-                        aria-expanded="false">Currículo</a>
-                    <ul class="dropdown-menu" aria-labelledby="dropdown1">
-                        <li class="dropdown-item" href="#"><a>Histórico Profissional</a></li>
-                        <li class="dropdown-item" href="#"><a>Histórico Acadêmico</a></li>
-                        <li class="dropdown-item dropdown">
-                            <a class="dropdown-toggle" id="dropdown1-1" data-toggle="dropdown" aria-haspopup="true"
-                                aria-expanded="false">Dropdown1.1</a>
-                            <ul class="dropdown-menu" aria-labelledby="dropdown1-1">
-                                <li class="dropdown-item" href="#"><a>Action 1.1</a></li>
-                                <li class="dropdown-item dropdown">
-                                    <a class="dropdown-toggle" id="dropdown1-1-1" data-toggle="dropdown"
-                                        aria-haspopup="true" aria-expanded="false">Dropdown1.1.1</a>
-                                    <ul class="dropdown-menu" aria-labelledby="dropdown1-1-1">
-                                        <li class="dropdown-item" href="#"><a>Action 1.1.1</a></li>
-                                    </ul>
-                                </li>
-                            </ul>
-                        </li>
-                    </ul>
-                </li>
-                <li class="nav-item dropdown @if($current=='produtos') active @endif ">
-                    <a class="nav-link dropdown-toggle" id="dropdown2" data-toggle="dropdown" aria-haspopup="true"
-                        aria-expanded="false">Dropdown2</a>
-                    <ul class="dropdown-menu" aria-labelledby="dropdown2">
-                        <li class="dropdown-item" href="#"><a>Action 2 A</a></li>
-                        <li class="dropdown-item" href="#"><a>Action 2 B</a></li>
-                        <li class="dropdown-item" href="#"><a>Action 2 C</a></li>
-                        <li class="dropdown-item dropdown">
-                            <a class="dropdown-toggle" id="dropdown2-1" data-toggle="dropdown" aria-haspopup="true"
-                                aria-expanded="false">Dropdown2.1</a>
-                            <ul class="dropdown-menu" aria-labelledby="dropdown2-1">
-                                <li class="dropdown-item" href="#"><a>Action 2.1 A</a></li>
-                                <li class="dropdown-item" href="#"><a>Action 2.1 B</a></li>
-                                <li class="dropdown-item" href="#"><a>Action 2.1 C</a></li>
-                                <li class="dropdown-item dropdown">
-                                    <a class="dropdown-toggle" id="dropdown2-1-1" data-toggle="dropdown"
-                                        aria-haspopup="true" aria-expanded="false">Dropdown2.1.1</a>
-                                    <ul class="dropdown-menu" aria-labelledby="dropdown2-1-1">
-                                        <li class="dropdown-item" href="#"><a>Action 2.1.1 A</a></li>
-                                        <li class="dropdown-item" href="#"><a>Action 2.1.1 B</a></li>
-                                        <li class="dropdown-item" href="#"><a>Action 2.1.1 C</a></li>
-                                    </ul>
-                                </li>
-                                <li class="dropdown-item dropdown">
-                                    <a class="dropdown-toggle" id="dropdown2-1-2" data-toggle="dropdown"
-                                        aria-haspopup="true" aria-expanded="false">Dropdown2.1.2</a>
-                                    <ul class="dropdown-menu" aria-labelledby="dropdown2-1-2">
-                                        <li class="dropdown-item" href="#"><a>Action 2.1.2 A</a></li>
-                                        <li class="dropdown-item" href="#"><a>Action 2.1.2 B</a></li>
-                                        <li class="dropdown-item" href="#"><a>Action 2.1.2 C</a></li>
-                                    </ul>
-                                </li>
-                            </ul>
-                        </li>
-                    </ul>
-                </li>
-            </ul>
-        </div>
+<nav class="navbar navbar-icon-top navbar-expand-lg navbar-dark bg-dark">
+    <a class="navbar-brand" href="/">Léo Carvalho</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item @if($current=='resume') active @endif ">
+                <a class="nav-link" href="#">
+                    <i class="fa fa-home"></i>
+                    Home
+                    <span class="sr-only">(current)</span>
+                </a>
+            </li>
+            <li class="nav-item @if($current=='resume') active @endif ">
+                <a class="nav-link" href="#">
+                    <i class="fa fa-envelope-o">
+                        <span class="badge badge-danger">11</span>
+                    </i>
+                    Link
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link disabled" href="#">
+                    <i class="fa fa-envelope-o">
+                        <span class="badge badge-warning">11</span>
+                    </i>
+                    Disabled
+                </a>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
+                    aria-haspopup="true" aria-expanded="false">
+                    <i class="fa fa-envelope-o">
+                        <span class="badge badge-primary">11</span>
+                    </i>
+                    Dropdown
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="#">Action</a>
+                    <a class="dropdown-item" href="#">Another action</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="#">Something else here</a>
+                </div>
+            </li>
+        </ul>
+        <ul class="navbar-nav ">
+            <li class="nav-item">
+                <a class="nav-link" href="#">
+                    <i class="fa fa-bell">
+                        <span class="badge badge-info">11</span>
+                    </i>
+                    Test
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">
+                    <i class="fa fa-globe">
+                        <span class="badge badge-success">11</span>
+                    </i>
+                    Test
+                </a>
+            </li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
     </div>
+</nav>
--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -1,77 +1,57 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta name="csrf-token" content="{{ csrf_token() }}" />
     <link rel="stylesheet" type="text/css" media="screen" href="{{ asset('css/app.css') }}" />
+    <link rel="stylesheet" type="text/css" media="screen" href="{{ asset('css/navbar.css') }}" />
     <script src="{{ asset('js/app.js') }}" type="text/javascript"></script>
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"
+        integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
 
     <style>
     body {
         padding: 20px;
     }
+
     .navbar {
         margin-bottom: 20px;
     }
     </style>
 
     <title>
-    @hasSection('title')
+        @hasSection('title')
         @yield('section')
-    @endif
+        @endif
     </title>
 
 </head>
+
 <body>
     <div class="container">
         @component('components.navbar', ["current" => $current])
         @endcomponent
-        <main  role="main">
-        @hasSection('body')
+        <main role="main">
+            @hasSection('body')
             @yield('body')
-        @endif
+            @endif
         </main>
     </div>
-    
+
     @hasSection('javascript')
-        @yield('javascript')
+    @yield('javascript')
     @endif
 
     <script type="text/javascript">
-    $(document).ready(function () {
-        $('.navbar .dropdown-item').on('click', function (e) {
-            var $el = $(this).children('.dropdown-toggle');
-            var $parent = $el.offsetParent(".dropdown-menu");
-            $(this).parent("li").toggleClass('open');
 
-            if (!$parent.parent().hasClass('navbar-nav')) {
-                if ($parent.hasClass('show')) {
-                    $parent.removeClass('show');
-                    $el.next().removeClass('show');
-                    $el.next().css({"top": -999, "left": -999});
-                } else {
-                    $parent.parent().find('.show').removeClass('show');
-                    $parent.addClass('show');
-                    $el.next().addClass('show');
-                    $el.next().css({"top": $el[0].offsetTop, "left": $parent.outerWidth() - 4});
-                }
-                e.preventDefault();
-                e.stopPropagation();
-            }
-        });
-
-        $('.navbar .dropdown').on('hidden.bs.dropdown', function () {
-            $(this).find('li.dropdown').removeClass('show open');
-            $(this).find('ul.dropdown-menu').removeClass('show open');
-        });
-
-    });
     </script>
 
     @component('components.footer')
     @endcomponent
-    
+
 </body>
+
 </html>
