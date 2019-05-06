3f59f86a0850d1f99e4489c7108f7d5503767ce9
Leonardo Carvalho <carvalho.csleo@gmail.com>
2019-05-05 21:51:29 -0300

    Add footer navbar and content license

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,29 @@
+<link rel="stylesheet" href="{{ asset('css/footer.css') }}">
+
+<div class="card text-center" id="footerMain">
+    <div class="card-footer" id="footerText">
+        <ul class="list-unstyled list-inline social text-center">
+            <li class="list-inline-item" id="footerSocial"><a href="https://www.linkedin.com/in/carvalholeo/"
+                    target="_blank"><i class="fab fa-linkedin"></i></a></li>
+            <li class="list-inline-item"><a href="https://github.com/carvalholeo/" target="_blank"><i
+                        class="fab fa-github" id="footerSocial"></i></a></li>
+            <li class="list-inline-item" id="footerSocial"><a href="https://twitter.com/_carvalho_leo"
+                    target="_blank"><i class="fab fa-twitter"></i></a></li>
+            <li class="list-inline-item" id="footerSocial"><a href="https://instagram.com/carvalho_leo"
+                    target="_blank"><i class="fab fa-instagram" id="footerInstagram"></i></a></li>
+            <li class="list-inline-item" id="footerSocial"><a href="/contact" target="_blank"><i
+                        class="fa fa-envelope"></i></a></li>
+        </ul>
+        <div class="text-muted">
+            <p><u><a href="/resume">Léo Carvalho</a></u> é desenvolvedor de sites e sistemas, com maior conhecimento em
+                PHP. Profissionalmente, trabalha como Instrutor de Treinamento.<a class="text-green ml-2"
+                    href="https://github.com/carvalholeo/site" target="_blank">Acesse o código fonte no GitHub</a></p>
+            </p>
+            <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/"><img alt="Licença Creative Commons"
+                    style="border-width:0" src="https://i.creativecommons.org/l/by-sa/4.0/88x31.png" /></a><br />Este
+            obra está licenciado com uma Licença <a rel="license"
+                href="http://creativecommons.org/licenses/by-sa/4.0/">Creative Commons Atribuição-CompartilhaIgual 4.0
+                Internacional</a>.
+        </div>
+    </div>
+</div>
--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -9,7 +9,7 @@
     <link rel="stylesheet" type="text/css" media="screen" href="{{ asset('css/app.css') }}" />
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
         integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
-        
+
     <link rel="stylesheet" type="text/css" media="screen" href="{{ asset('css/navbar.css') }}" />
     <script src="{{ asset('js/app.js') }}" type="text/javascript"></script>
     <script src="https://code.jquery.com/jquery-3.4.1.min.js"
@@ -51,10 +51,10 @@
     <script type="text/javascript">
 
     </script>
-
-    @component('components.footer')
-    @endcomponent
-
+    <footer class="footer navbar-fixed-bottom">
+        @component('components.footer')
+        @endcomponent
+    </footer>
 </body>
 
 </html>
