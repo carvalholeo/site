649bafac7a68f0a810deca8a40779f0a797fdb99
Leonardo Carvalho <carvalho.csleo@gmail.com>
2019-05-08 04:02:52 -0300

    Fix Google Analytics code

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -32,7 +32,7 @@
 
 @else
 <!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-123363962-1"></script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-139872605-1"></script>
 <script>
 window.dataLayer = window.dataLayer || [];
 
@@ -40,6 +40,6 @@ function gtag() {
     dataLayer.push(arguments);
 }
 gtag('js', new Date());
-gtag('config', 'UA-123363962-1');
+gtag('config', 'UA-139872605-1');
 </script>
 @endenv
