8d9a61e3ccdcd5ec027608aae5c51330d755e432
Leonardo Carvalho <carvalho.csleo@gmail.com>
2019-05-12 01:26:44 -0300

    Add Construction view and its gif

---
+++ resources/views/construction.blade.php
@@ -0,0 +1,25 @@
+@extends('layout.app', ["current" => "resume"])
+
+@section('body')
+<body>
+    <div class="container">
+        <div class="row text-center">
+            <div class="col-lg-6 offset-lg-3 col-sm-6 offset-sm-3 col-12 p-3 error-main">
+                <div class="row">
+                    <div class="col-lg-8 col-12 col-sm-10 offset-lg-2 offset-sm-1">
+                        <h1 class="h1">Quase lá!</h1>
+                        <h5 class="h4">Ainda estamos em construção do site.</h4>
+
+                            <div class="text-center">
+                                <img src="{{ asset('storage/images/construction.gif') }}" class="rounded img-fluid"
+                                    alt="...">
+                            </div>
+
+                            <p class="text-muted">Em breve isso aqui ficará parecendo um grande jardim florido, mas, por enquanto, não temos nada além desse gif maroto :-P</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+@endsection
