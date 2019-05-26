0e7dffa60e55d8b8ce9722a1ac15e67817a32c30
Leonardo Carvalho <carvalho.csleo@gmail.com>
2019-05-26 16:56:51 -0300

    Add Pinterest tag

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -3,6 +3,7 @@
 
 <head>
     <meta charset="utf-8" />
+    <meta name="p:domain_verify" content="cc355e0c54673679bbb76cccfc621d0c"/>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta name="csrf-token" content="{{ csrf_token() }}" />
