5c72ba7c2155aa3322183f6f8f839e0e3c76c15c
Leo Carvalho <carvalho.csleo@gmail.com>
2019-07-27 03:56:55 -0300

    Fix Recaptcha bug

--- public/js/captcha.js
+++ public/js/captcha.js
@@ -1,28 +1,12 @@
 $(document).ready(function() {
     grecaptcha.ready(function() {
-        grecaptcha.execute('6LdoxaYUAAAAAJx8MEuu-K5-AeStQBO9l8yBPpyq', {action: 'contact'}).then(function(token) {
-            var submitButton = document.getElementById('submit');
-            submitButton.disabled = false;
-            
-            $.ajaxSetup({
-                headers: {
-                    'X-CSRF-TOKEN': "{{ csrf_token() }}"
-                }
+        grecaptcha.execute('6LdoxaYUAAAAAJx8MEuu-K5-AeStQBO9l8yBPpyq', {action: 'contact'})
+            .then(function(token) {
+                document.getElementById('submit').disabled = false;
+                
+                var recaptchaResponse = document.getElementById('recaptchaResponse');
+                recaptchaResponse.value = token;
+
             });
-
-            $.ajax({
-                type: 'OPTIONS',
-                url: apiRoute(),
-                context: this,
-                data: plano,
-                success: function (data) {
-                    
-                },
-                error: function (error) {
-
-                }
-            });
-
-        });
     });
 });
--- resources/views/contact/index.blade.php
+++ resources/views/contact/index.blade.php
@@ -23,32 +23,34 @@
     </p>
     <hr />
  
-    <form action="/contact" method="POST" id="contactForm">
-      <input type="hidden" name="_token" value="{{ csrf_token() }}">
+    <form method="POST" id="contactForm">
+        <input type="hidden" name="_token" value="{{ csrf_token() }}">
+    
+        <div class="form-group">
+            <label for="nome">Seu nome</label>
+            <input type="text" id="nome" name="nome" class="form-control" placeholder="Nome">
+        </div>
  
-      <div class="form-group">
-        <label for="nome">Seu nome</label>
-        <input type="text" id="nome" name="nome" class="form-control" placeholder="Nome">
-      </div>
+        <div class="form-group">
+            <label for="email">E-Mail</label>
+            <input type="text" id="email" name="email" class="form-control" placeholder="E-Mail">
+        </div>
  
-      <div class="form-group">
-        <label for="email">E-Mail</label>
-        <input type="text" id="email" name="email" class="form-control" placeholder="E-Mail">
-      </div>
- 
-      <div class="form-group">
-        <textarea id="mensagem" name="mensagem" class="form-control" placeholder="Digite sua mensagem"></textarea>
-      </div>
- 
-      <button type="submit" class="btn btn-primary" id="submit" disabled>Enviar</button>
-      <button type="cancel" class="btn btn-danger" id="cancel">Apagar</button>
+        <div class="form-group">
+            <textarea id="mensagem" name="mensagem" class="form-control" placeholder="Digite sua mensagem"></textarea>
+        </div>
+    
+        <input type="hidden" name="recaptcha_response" id="recaptchaResponse">
+
+        <button type="submit" class="btn btn-primary" id="submit" disabled>Enviar</button>
+        <button type="cancel" class="btn btn-danger" id="cancel">Apagar</button>
  
     </form>
  
 </div>
 
+<script src="https://www.google.com/recaptcha/api.js?render=6LdoxaYUAAAAAJx8MEuu-K5-AeStQBO9l8yBPpyq"></script>
+
+<script src="{{ asset('js/captcha.js') }}" type="text/javascript"></script>
 
 @endsection
-
-<script src="https://www.google.com/recaptcha/api.js?render=6LdoxaYUAAAAAJx8MEuu-K5-AeStQBO9l8yBPpyq"></script>
-<script src="{{ asset('js/captcha.js') }}" type="text/javascript"></script>
