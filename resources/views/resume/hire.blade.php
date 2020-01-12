551a17dc8cdd821d7c607590905701d11506ebd6
Leo Carvalho <carvalho.csleo@gmail.com>
2020-01-12 02:29:12 -0300

    Release first preview version about new layout

---
+++ resources/views/resume/hire.blade.php
@@ -0,0 +1,12 @@
+<section class="ftco-section ftco-hireme img" style="background-image: url({{ asset('storage/images/bg_1.jpg') }})">
+    <div class="overlay"></div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-7 ftco-animate text-center">
+                <h2>Estou <span>disponível</span> para novas oportunidades</h2>
+                <p>Estou aberto para propostas de novos desafios em trabalho formal ou freelancer. Vamos bater um papo mais aprofundado sobre sua necessidade?</p>
+                <p class="mb-0"><a href="#contact-section" class="btn btn-primary py-3 px-5">Contrate-me</a></p>
+            </div>
+        </div>
+    </div>
+</section>
