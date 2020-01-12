ff4c6160b41a08f8cc140b4a584ff2b6a012da83
Leo Carvalho <carvalho.csleo@gmail.com>
2020-01-12 12:57:31 -0300

    Create Education Model to manipulate it on DB

---
+++ database/migrations/2020_01_12_153149_create_education_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEducationTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('educations', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('degree', 50);
+            $table->string('title', 50);
+            $table->string('instution', 50);
+            $table->string('instutionLink', 50);
+            $table->date('startDate');
+            $table->date('endDate')->nullable();
+            $table->boolean('isFinished');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('educations');
+    }
+}
