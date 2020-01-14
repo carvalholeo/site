1ebe78786e329216e888b71159c5464d00e36481
Leo Carvalho <carvalho.csleo@gmail.com>
2020-01-14 16:49:15 -0300

    Change text on hire section

--- resources/views/resume/hire.blade.php
+++ resources/views/resume/hire.blade.php
@@ -3,8 +3,8 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-7 ftco-animate text-center">
-                <h2>Estou <span>disponível</span> para novas oportunidades</h2>
-                <p>Estou aberto para propostas de novos desafios em trabalho formal ou freelancer. Vamos bater um papo mais aprofundado sobre sua necessidade?</p>
+                <h2>Estou <span>disponível</span> para contratação</h2>
+                <p>Estou aberto para propostas de novos desafios, sejam eles em trabalhos formais ou freelancer. Vamos bater um papo mais aprofundado sobre sua necessidade?</p>
                 <p class="mb-0"><a href="#contact-section" class="btn btn-primary py-3 px-5">Contrate-me</a></p>
             </div>
         </div>
