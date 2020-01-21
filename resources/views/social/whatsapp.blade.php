9efd148a1dcd6b2f41ce5463cb21b5240190edf9
Leo Carvalho <carvalho.csleo@gmail.com>
2020-01-21 20:33:34 -0300

    Fix WhatsApp Account number on its view

--- resources/views/social/whatsapp.blade.php
+++ resources/views/social/whatsapp.blade.php
@@ -10,7 +10,7 @@ Aguarde um instante, você já está indo para o meu chat do WhatsApp!
 
 @section('javascript')
     <script type="text/javascript">
-        url = "http://wa.me/55139967375760?source=site&text=Vamos falar de negócios!";
+        url = "http://wa.me/5513997375760?source=site&text=Vamos falar de negócios!";
         redirect(url);
     </script>
 @endsection
