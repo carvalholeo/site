dfefd369f810371202defd8f156a0c19f9bf4d59
Leo Carvalho <carvalho.csleo@gmail.com>
2020-01-23 13:05:23 -0300

    Add sitemap to site

--- app/Http/Controllers/InternalController.php
+++ app/Http/Controllers/InternalController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App;
+use Illuminate\Support\Facades\URL;
 
 class InternalController extends Controller
 {
@@ -19,5 +21,33 @@ class InternalController extends Controller
             return response('Erro interno. Não foi possível atualizar o site.', 503);
         }
         return response('Enviado com sucesso. Atualizará em breve.', 201);
+    }
+
+    public function sitemap()
+    {
+        $sitemap = App::make('sitemap');
+
+        $sitemap->setCache('laravel.sitemap', 60);
+
+        if (!$sitemap->isCached()) {
+            $sitemap->add(URL::to(route('index'), [], true), '2020-01-14T17:10:00-03:00', '1.0', 'weekly');
+            $sitemap->add(URL::to(route('resume.index'), [], true), '2020-01-14T17:10:00-03:00', '0.8', 'monthly');
+
+            $sitemap->add(URL::to(route('social.index'), [], true), '2020-01-14T17:10:00-03:00', '0.5', 'yearly');
+            $sitemap->add(URL::to(route('social.facebook'), [], true), '2020-01-14T17:10:00-03:00', '0.5', 'yearly');
+            $sitemap->add(URL::to(route('social.youtube'), [], true), '2020-01-14T17:10:00-03:00', '0.5', 'yearly');
+            $sitemap->add(URL::to(route('social.blog'), [], true), '2020-01-14T17:10:00-03:00', '0.5', 'yearly');
+            $sitemap->add(URL::to(route('social.twitter'), [], true), '2020-01-14T17:10:00-03:00', '0.5', 'yearly');
+            $sitemap->add(URL::to(route('social.instagram'), [], true), '2020-01-14T17:10:00-03:00', '0.5', 'yearly');
+            $sitemap->add(URL::to(route('social.linkedin'), [], true), '2020-01-14T17:10:00-03:00', '0.5', 'yearly');
+            $sitemap->add(URL::to(route('social.wordpress'), [], true), '2020-01-14T17:10:00-03:00', '0.5', 'yearly');
+            $sitemap->add(URL::to(route('social.pinterest'), [], true), '2020-01-14T17:10:00-03:00', '0.5', 'yearly');
+            $sitemap->add(URL::to(route('social.telegram'), [], true), '2020-01-14T17:10:00-03:00', '0.5', 'yearly');
+            $sitemap->add(URL::to(route('social.stackoverflow'), [], true), '2020-01-14T17:10:00-03:00', '0.5', 'yearly');
+            $sitemap->add(URL::to(route('social.whatsapp'), [], true), '2020-01-14T17:10:00-03:00', '0.5', 'yearly');
+
+        }
+
+        return $sitemap->render('xml');
     }
 }
