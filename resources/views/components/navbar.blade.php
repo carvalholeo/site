d0aa53819f892ffe83e0d395ac29527e3a7478d1
Leo Carvalho <carvalho.csleo@gmail.com>
2020-01-30 01:08:37 -0300

    Remove trailing slash from navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -7,13 +7,13 @@
 
       <div class="collapse navbar-collapse" id="ftco-nav">
         <ul class="navbar-nav nav ml-auto">
-          <li class="nav-item"><a href="{{ route('index')}}/#home-section" class="nav-link"><span>Home</span></a></li>
-          <li class="nav-item"><a href="{{ route('index')}}/#about-section" class="nav-link"><span>Sobre</span></a></li>
-          <li class="nav-item"><a href="{{ route('index')}}/#resume-section" class="nav-link"><span>Currículo</span></a></li>
-          <!--li class="nav-item"><a href="{{ route('index')}}/#services-section" class="nav-link"><span>Serviços</span></a></li>
-          <li class="nav-item"><a href="{{ route('index')}}/#projects-section" class="nav-link"><span>Projetos</span></a></li>
-          <li class="nav-item"><a href="{{ route('index')}}/#blog-section" class="nav-link"><span>Blog</span></a></li-->
-          <li class="nav-item"><a href="{{ route('index')}}/#contact-section" class="nav-link"><span>Contato</span></a></li>
+          <li class="nav-item"><a href="{{ route('index')}}#home-section" class="nav-link"><span>Home</span></a></li>
+          <li class="nav-item"><a href="{{ route('index')}}#about-section" class="nav-link"><span>Sobre</span></a></li>
+          <li class="nav-item"><a href="{{ route('index')}}#resume-section" class="nav-link"><span>Currículo</span></a></li>
+          <!--li class="nav-item"><a href="{{ route('index')}}#services-section" class="nav-link"><span>Serviços</span></a></li>
+          <li class="nav-item"><a href="{{ route('index')}}#projects-section" class="nav-link"><span>Projetos</span></a></li>
+          <li class="nav-item"><a href="{{ route('index')}}#blog-section" class="nav-link"><span>Blog</span></a></li-->
+          <li class="nav-item"><a href="{{ route('index')}}#contact-section" class="nav-link"><span>Contato</span></a></li>
         </ul>
       </div>
     </div>
