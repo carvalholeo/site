04d66ccceb19ad5bbc32e6c83506296addbe96a0
Leo Carvalho <carvalho.csleo@gmail.com>
2020-01-30 01:36:53 -0300

    Translate and add a CTA button to Blog view

--- resources/views/resume/blog.blade.php
+++ resources/views/resume/blog.blade.php
@@ -3,8 +3,9 @@
       <div class="row justify-content-center mb-5 pb-5">
         <div class="col-md-7 heading-section text-center ftco-animate">
           <h1 class="big big-2">Blog</h1>
-          <h2 class="mb-4">Our Blog</h2>
-          <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia</p>
+          <h2 class="mb-4">Blog</h2>
+          <p>Um local para compartilhar conhecimento com a comunidade dev. Este contém apenas conteúdos profissionais.</p>
+          <p>Veja abaixo os últimos posts publicados.</p>
         </div>
       </div>
       <div class="row d-flex">
@@ -18,11 +19,12 @@
               <div class="d-flex align-items-center mb-3 meta">
                   <p class="mb-0">
                       <span class="mr-2">{{ \Carbon\Carbon::parse($post->pubDate)->format('d M y')}}</span>
-                      <a href="#" class="mr-2">{{ $post->author }}</a>
+                      <span class="mr-2">Autor: </span><a href="#" class="mr-2">{{ $post->author }}</a>
                   </p>
               </div>
               <p> {{ $post->description }}</p>
 
+              <a role="button" class="btn btn-primary btn-sm" href="{{ $post->link }}">Continue lendo...</a>
               <p class="text-muted">Tags:
                 @foreach ($post->categories as $category)
                   {{ $category }}
