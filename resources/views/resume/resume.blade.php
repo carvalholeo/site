7a4b47a40ab53aa9caa77377b795e67a93273240
Leo Carvalho <carvalho.csleo@gmail.com>
2020-02-05 23:40:49 -0300

    Adjust course layout and fix divs bugs

--- resources/views/resume/resume.blade.php
+++ resources/views/resume/resume.blade.php
@@ -11,11 +11,11 @@
                       <li><a href="#volunteer">Trabalhos voluntários</a></li>
                       <li><a href="#certifications">Certificações</a></li>
                     </ul>
-                  </nav>
-                </div>
-                <div class="col-md-9">
-                  <div id="education" class= "page one">
-                      <h2 class="heading">Educação</h2>
+                </nav>
+            </div>
+            <div class="col-md-9">
+                <div id="education" class= "page one">
+                    <h2 class="heading">Educação</h2>
 @for ($i = 0; $i < 1; $i++)
                     <div class="resume-wrap d-flex ftco-animate">
                         <div class="icon d-flex align-items-center justify-content-center">
@@ -45,8 +45,9 @@
                         <a role="button" type="button" class="btn btn-primary btn-lg" href="{{ route('resume.education') }}">Ver mais experiências acadêmicas</a>
                     </div>
                 </div>
-                  <div id="experience" class= "page two">
-                      <h2 class="heading">Experiência</h2>
+
+                <div id="experience" class= "page two">
+                    <h2 class="heading">Experiência</h2>
 @for ($i = 0; $i < 1; $i++)
                     <div class="resume-wrap d-flex ftco-animate">
                         <div class="icon d-flex align-items-center justify-content-center">
@@ -58,164 +59,162 @@
                             <a href="{{ $xps[$i]->companyLink }}" target="_blank">
                                 <span class="position">{{ $xps[$i]->company }}</span>
                             </a>
-                        <p>{{ $xps[$i]->description }}</p>
+                            <p>{{ $xps[$i]->description }}</p>
                         </div>
                     </div>
 @endfor
                     <div class="justify-content-center text-center">
                         <a role="button" type="button" class="btn btn-primary btn-lg" href="{{ route('resume.professional') }}">Ver mais experiências profissionais</a>
                     </div>
-                  </div>
-                  <div id="skills" class= "page three">
-                      <h2 class="heading">Habilidades</h2>
+                </div>
 
+                <div id="skills" class= "page three">
+                    <h2 class="heading">Habilidades</h2>
 
-                        <!-- Linguagens no último ano -->
-                        <h3>Linguagens utilizadas no último ano</h3>
-                        <figure>
-                            <embed src="https://wakatime.com/share/@3d0d5dec-46a0-4f76-a0c9-5cdb5a198df0/3efee2bd-2480-4041-9c97-c2aed1b22075.svg"></embed>
-                        </figure>
+                    <!-- Linguagens no último ano -->
+                    <h3>Linguagens utilizadas no último ano</h3>
+                    <figure>
+                        <embed src="https://wakatime.com/share/@3d0d5dec-46a0-4f76-a0c9-5cdb5a198df0/3efee2bd-2480-4041-9c97-c2aed1b22075.svg"></embed>
+                    </figure>
 
+                    <!-- Linguagens no último mês -->
+                    <h3>Linguagens utilizadas nos últimos 30 dias</h3>
+                    <figure>
+                        <embed src="https://wakatime.com/share/@3d0d5dec-46a0-4f76-a0c9-5cdb5a198df0/705b41e3-76ab-4085-b1f2-ac95bf7cb48a.svg"></embed>
+                    </figure>
 
-                        <!-- Linguagens no último mês -->
-                        <h3>Linguagens utilizadas nos últimos 30 dias</h3>
-                        <figure>
-                            <embed src="https://wakatime.com/share/@3d0d5dec-46a0-4f76-a0c9-5cdb5a198df0/705b41e3-76ab-4085-b1f2-ac95bf7cb48a.svg"></embed>
-                        </figure>
+                    <!-- Linguagens na última semana -->
+                    <h3>Linguagens utilizadas nos últimos 7 dias</h3>
+                    <figure>
+                        <embed src="https://wakatime.com/share/@3d0d5dec-46a0-4f76-a0c9-5cdb5a198df0/90eb9432-bded-4d25-a707-08b9668a8d9a.svg"></embed>
+                    </figure>
 
-
-                        <!-- Linguagens na última semana -->
-                        <h3>Linguagens utilizadas nos últimos 7 dias</h3>
-                        <figure>
-                          <embed src="https://wakatime.com/share/@3d0d5dec-46a0-4f76-a0c9-5cdb5a198df0/90eb9432-bded-4d25-a707-08b9668a8d9a.svg"></embed>
-                        </figure>
-
-                        <h3>Idiomas</h3>
-                        <div class="row">
-                            <div class="col-md-6 animate-box">
-                                <div class="progress-wrap ftco-animate">
-                                    <h3>Português</h3>
-                                    <div class="progress">
-                                        <div class="progress-bar color-1" role="progressbar" aria-valuenow="100"
-                                          aria-valuemin="0" aria-valuemax="100" style="width:100%">
-                                            <span>100%</span>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div class="col-md-6 animate-box">
-                                <div class="progress-wrap ftco-animate">
-                                    <h3>English</h3>
-                                    <div class="progress">
-                                         <div class="progress-bar color-2" role="progressbar" aria-valuenow="50"
-                                          aria-valuemin="0" aria-valuemax="100" style="width:50%">
-                                        <span>50%</span>
-                                          </div>
+                    <h3>Idiomas</h3>
+                    <div class="row">
+                        <div class="col-md-6 animate-box">
+                            <div class="progress-wrap ftco-animate">
+                                <h3>Português</h3>
+                                <div class="progress">
+                                    <div class="progress-bar color-1" role="progressbar" aria-valuenow="100"
+                                        aria-valuemin="0" aria-valuemax="100" style="width:100%">
+                                        <span>100%</span>
                                     </div>
                                 </div>
                             </div>
                         </div>
+
+                        <div class="col-md-6 animate-box">
+                            <div class="progress-wrap ftco-animate">
+                                <h3>English</h3>
+                                <div class="progress">
+                                        <div class="progress-bar color-2" role="progressbar" aria-valuenow="50"
+                                        aria-valuemin="0" aria-valuemax="100" style="width:50%">
+                                    <span>50%</span>
+                                        </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    <div id="courses" class= "page four">
-                        <h2 class="heading">Cursos e Bootcamps</h2>
-@foreach ($courses as $course)
+                </div>
+
+                <div id="courses" class="page five">
+                    <h2 class="heading">Cursos e Bootcamps</h2>
+@for ($i = 0; $i < 3; $i++)
                     <div class="resume-wrap d-flex ftco-animate">
                         <div class="icon d-flex align-items-center justify-content-center">
                             <span class="flaticon-ideas"></span>
                         </div>
                         <div class="text pl-3">
-                            <span class="date">{{ \Carbon\Carbon::parse($course->startDate)->format('d/M/y') }} -
-                                @if ($course->isFinished != 0)
-                                    {{ \Carbon\Carbon::parse($course->endDate)->format('d/M/y') }}
+                            <span class="date">{{ \Carbon\Carbon::parse($courses[$i]->startDate)->format('d/M/y') }} -
+                                @if ($courses[$i]->isFinished != 0)
+                                    {{ \Carbon\Carbon::parse($courses[$i]->endDate)->format('d/M/y') }}
                                 @else
                                     Atualmente
                                 @endif
                             </span>
 
-                            <h3 class="position">{{ $course->typeOfAward }}
-                                @if($course->workLoad != 0)
-                                 - {{ $course->workLoad }}h
+                            <h3 class="position">{{ $courses[$i]->typeOfAward }}
+                                @if($courses[$i]->workLoad != 0)
+                                - {{ $courses[$i]->workLoad }}h
                                 @endIf
                             </h3>
 
-                            <a href="{{ $course->awardLink }}" target="_blank">
-                                <h2>{{ $course->awardName }}</h2>
+                            <a href="{{ $courses[$i]->awardLink }}" target="_blank">
+                                <h2>{{ $courses[$i]->awardName }}</h2>
                             </a>
-                            <a href="{{ $course->institutionLink }}" target="_blank">
-                                <span class="position">{{ $course->institutionName }}</span>
+                            <a href="{{ $courses[$i]->institutionLink }}" target="_blank">
+                                <span class="position">{{ $courses[$i]->institutionName }}</span>
                             </a>
-                            <p>{{ $course->description }}</p>
+                            <p>{{ $courses[$i]->description }}</p>
 
-                            @if ($course->awardNumber != null)
-                                <p>Número do certificado: {{ $course->awardNumber}}</p>
+                            @if ($courses[$i]->awardNumber != null)
+                                <p>Número do certificado: {{ $courses[$i]->awardNumber}}</p>
                             @endif
 
                         </div>
                     </div>
-@endforeach
-                  </div>
-
-                  <div id="volunteer" class= "page five">
-                    <h2 class="heading">Trabalhos voluntários</h2>
-@for ($i = 0; $i < 2; $i++)
-                <div class="resume-wrap d-flex ftco-animate">
-                    <div class="icon d-flex align-items-center justify-content-center">
-                        <span class="flaticon-ideas"></span>
-                    </div>
-                    <div class="text pl-3">
-                        <span class="date">{{ \Carbon\Carbon::parse($volunteers[$i]->startDate)->format('d/M/y') }} -
-                            @if ($volunteers[$i]->isFinished != 0)
-                                {{ \Carbon\Carbon::parse($volunteers[$i]->endDate)->format('d/M/y') }}
-                            @else
-                                Atualmente
-                            @endif
-                        </span>
-
-                        <h2>{{ $volunteers[$i]->position }}</h2>
-                        <a href="{{ $volunteers[$i]->institutionLink }}" target="_blank">
-                            <span class="position">{{ $volunteers[$i]->institutionName }}</span>
-                        </a>
-                        <p>{{ $volunteers[$i]->description }}</p>
-                    </div>
-                </div>
 @endfor
-              </div>
-
-              <div id="certifications" class= "page six">
-                <h2 class="heading">Certificações</h2>
-@for ($i = 0; $i < 2; $i++)
-            <div class="resume-wrap d-flex ftco-animate">
-                <div class="icon d-flex align-items-center justify-content-center">
-                    <span class="flaticon-ideas"></span>
                 </div>
-                <div class="text pl-3">
-                    <span class="date">{{ \Carbon\Carbon::parse($certifications[$i]->startDate)->format('d/M/y') }} - {{ \Carbon\Carbon::parse($certifications[$i]->endDate)->format('d/M/y') }}
-                    </span>
 
-                    <h3 class="position">{{ $certifications[$i]->certType }}
-                        @if($certifications[$i]->workLoad != 0)
-                         - {{ $certifications[$i]->workLoad }}h
-                        @endIf
-                    </h3>
+                <div id="volunteer" class= "page five">
+                <h2 class="heading">Trabalhos voluntários</h2>
+@for ($i = 0; $i < 2; $i++)
+                    <div class="resume-wrap d-flex ftco-animate">
+                        <div class="icon d-flex align-items-center justify-content-center">
+                            <span class="flaticon-ideas"></span>
+                        </div>
+                        <div class="text pl-3">
+                            <span class="date">{{ \Carbon\Carbon::parse($volunteers[$i]->startDate)->format('d/M/y') }} -
+                                @if ($volunteers[$i]->isFinished != 0)
+                                    {{ \Carbon\Carbon::parse($volunteers[$i]->endDate)->format('d/M/y') }}
+                                @else
+                                    Atualmente
+                                @endif
+                            </span>
 
-                    <a href="{{ $certifications[$i]->certLink }}" target="_blank">
-                        <h2>{{ $certifications[$i]->certName }}</h2>
-                    </a>
-                    <a href="{{ $certifications[$i]->issuerLink }}" target="_blank">
-                        <span class="position">{{ $certifications[$i]->issuerName }}</span>
-                    </a>
-                    <p>{{ $certifications[$i]->description }}</p>
+                            <h2>{{ $volunteers[$i]->position }}</h2>
+                            <a href="{{ $volunteers[$i]->institutionLink }}" target="_blank">
+                                <span class="position">{{ $volunteers[$i]->institutionName }}</span>
+                            </a>
+                            <p>{{ $volunteers[$i]->description }}</p>
+                        </div>
+                    </div>
+@endfor
+                </div>
+                <div id="certifications" class= "page six">
+                    <h2 class="heading">Certificações</h2>
+@for ($i = 0; $i < 2; $i++)
+                    <div class="resume-wrap d-flex ftco-animate">
+                        <div class="icon d-flex align-items-center justify-content-center">
+                            <span class="flaticon-ideas"></span>
+                        </div>
+                        <div class="text pl-3">
+                            <span class="date">{{ \Carbon\Carbon::parse($certifications[$i]->startDate)->format('d/M/y') }} - {{ \Carbon\Carbon::parse($certifications[$i]->endDate)->format('d/M/y') }}
+                            </span>
 
-                    @if ($certifications[$i]->certNumber != null)
-                        <p>Número do certificado: {{ $certifications[$i]->certNumber}}</p>
-                    @endif
+                            <h3 class="position">{{ $certifications[$i]->certType }}
+                                @if($certifications[$i]->workLoad != 0)
+                                - {{ $certifications[$i]->workLoad }}h
+                                @endIf
+                            </h3>
 
+                            <a href="{{ $certifications[$i]->certLink }}" target="_blank">
+                                <h2>{{ $certifications[$i]->certName }}</h2>
+                            </a>
+                            <a href="{{ $certifications[$i]->issuerLink }}" target="_blank">
+                                <span class="position">{{ $certifications[$i]->issuerName }}</span>
+                            </a>
+                            <p>{{ $certifications[$i]->description }}</p>
+
+                            @if ($certifications[$i]->certNumber != null)
+                                <p>Número do certificado: {{ $certifications[$i]->certNumber}}</p>
+                            @endif
+
+                        </div>
+                    </div>
+@endfor
                 </div>
             </div>
-@endfor
-          </div>
-                </div>
-          </div>
+        </div>
     </div>
 </section>
