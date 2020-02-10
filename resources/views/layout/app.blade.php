af3bfe06bbf7c84f4c9daa2f2c8e3c0c229f6702
Leo Carvalho <carvalho.csleo@gmail.com>
2020-02-10 20:51:11 -0300

    Add Query String option to avoid FOIT in font-face

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -16,7 +16,7 @@
 
 	<meta name="csrf-token" content="{{ csrf_token() }}" />
 
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900" media="none" onload="if(media!='all')media='all'">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap" media="none" onload="if(media!='all')media='all'">
 
     <link rel="stylesheet" href="{{ mix('css/all.css') }}">
 
