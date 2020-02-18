1e4213e11a0081743bc6c898699e5cb31ed1c29b
Leo Carvalho <carvalho.csleo@gmail.com>
2020-02-17 23:23:10 -0300

    Add Google Tag Manager

--- resources/views/layout/detail.blade.php
+++ resources/views/layout/detail.blade.php
@@ -2,6 +2,14 @@
 <html lang="pt">
 
 <head>
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-PG7DJZC');</script>
+    <!-- End Google Tag Manager -->
+
     <link rel="preconnect" href="//fonts.gstatic.com">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://www.gstatic.com">
@@ -39,6 +47,11 @@
 </head>
 
 <body data-spy="scroll" data-target=".site-navbar-target" data-offset="300">
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PG7DJZC"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
+
     <div class="container">
         @component('components.navbar')
         @endcomponent
