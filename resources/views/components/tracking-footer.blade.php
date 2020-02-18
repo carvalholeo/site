a643e029042c37d84503b886ed4c36ab9a4adb89
Leo Carvalho <carvalho.csleo@gmail.com>
2020-02-18 15:20:15 -0300

    Refactor common scripts at layout base

---
+++ resources/views/components/tracking-footer.blade.php
@@ -0,0 +1,50 @@
+<script async src="{{ mix('js/all.js') }}"></script>
+<script data-ad-client="ca-pub-1577931231143329" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+
+@env('production')
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-139872605-1"></script>
+
+<script>
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag() {
+        dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+    gtag('config', 'UA-139872605-1', {
+        'link_attribution': {
+            'cookie_name': '_main',
+            'cookie_domain': 'leocarvalho.dev',
+            'cookie_expires': 0,
+            'levels': 15
+        },
+        'anonymize_ip': true
+    );
+    if (window.performance) {
+        // Gets the number of milliseconds since page load
+        // (and rounds the result since the value must be an integer).
+        let timeSincePageLoad = Math.round(performance.now());
+
+        // Sends the timing event to Google Analytics.
+        gtag('event', 'timing_complete', {
+            'name': 'load',
+            'value': timeSincePageLoad,
+            'event_category': 'JS Dependencies'
+        });
+    }
+</script>
+
+<script>
+    var $buoop = {required:{e:-4,f:-3,o:-3,s:-1,c:-3},insecure:true,api:2020.02 };
+    function $buo_f(){
+     var e = document.createElement("script");
+     e.src = "//browser-update.org/update.min.js";
+     document.body.appendChild(e);
+    };
+    try {document.addEventListener("DOMContentLoaded", $buo_f,false)}
+    catch(e){window.attachEvent("onload", $buo_f)}
+</script>
+
+@endenv
--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -2,41 +2,7 @@
 <html lang="pt">
 
 <head>
-
-    <link rel="preconnect" href="//fonts.gstatic.com">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://www.gstatic.com">
-    <link rel="preconnect" href="https://www.google.com">
-    <link rel="preconnect" href="//browser-update.org">
-    <link rel="preconnect" href="//www.googletagmanager.com">
-    <link rel="preconnect" href="//wakatime.com">
-    <link rel="preconnect" href="https://pagead2.googlesyndication.com">
-    <link rel="preconnect" href="https://tpc.googlesyndication.com">
-    <link rel="preconnect" href="https://www.google-analytics.com">
-    <link rel="preconnect" href="https://stats.g.doubleclick.net">
-    <script async src="{{ mix('js/custom.js') }}"></script>
-    <script async src="{{ mix('js/lazysize.js') }}"></script>
-    @env('production')
-    {!! SEO::generate(true) !!}
-    @endenv
-
-    @env('local')
-    {!! SEO::generate() !!}
-    @endenv
-
-    <meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-	<meta name="csrf-token" content="{{ csrf_token() }}" />
-
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap" media="none" onload="if(media!='all')media='all'">
-
-    <link rel="stylesheet" href="{{ mix('css/all.css') }}">
-
-    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('/favicon.ico') }}">
-    <link rel="apple-touch-icon" href="{{ asset('/icon.png') }}">
-    <link rel="apple-touch-icon-precomposed" href="{{ asset('/icon.png') }}">
-    <meta name="msapplication-TileImage" href="{{ asset('/icon.png') }}">
+    @include('components.tracking-head')
 </head>
 
 <body data-spy="scroll" data-target=".site-navbar-target" data-offset="300">
@@ -64,56 +30,7 @@
         @endif
     </footer>
 
-<script async src="{{ mix('js/all.js') }}"></script>
-<script data-ad-client="ca-pub-1577931231143329" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-
-@env('production')
-
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-139872605-1"></script>
-
-<script>
-    window.dataLayer = window.dataLayer || [];
-
-    function gtag() {
-        dataLayer.push(arguments);
-    }
-    gtag('js', new Date());
-    gtag('config', 'UA-139872605-1', {
-        'link_attribution': {
-            'cookie_name': '_main',
-            'cookie_domain': 'leocarvalho.dev',
-            'cookie_expires': 0,
-            'levels': 15
-        },
-        'anonymize_ip': true
-    );
-    if (window.performance) {
-        // Gets the number of milliseconds since page load
-        // (and rounds the result since the value must be an integer).
-        let timeSincePageLoad = Math.round(performance.now());
-
-        // Sends the timing event to Google Analytics.
-        gtag('event', 'timing_complete', {
-            'name': 'load',
-            'value': timeSincePageLoad,
-            'event_category': 'JS Dependencies'
-        });
-    }
-</script>
-
-<script>
-    var $buoop = {required:{e:-4,f:-3,o:-3,s:-1,c:-3},insecure:true,api:2020.02 };
-    function $buo_f(){
-     var e = document.createElement("script");
-     e.src = "//browser-update.org/update.min.js";
-     document.body.appendChild(e);
-    };
-    try {document.addEventListener("DOMContentLoaded", $buo_f,false)}
-    catch(e){window.attachEvent("onload", $buo_f)}
-</script>
-
-@endenv
+@include('components.tracking-footer')
 
 </body>
 
